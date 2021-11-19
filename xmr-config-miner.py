347e21b0a3cc88c16c8b1310f9a1ae2e96d3984b
Pascal Masny <pascal.masny@protonmail.com>
2021-11-19 22:37:52 +0100

    #3 & #4 intigration ready and done

--- xmr-config-miner.py
+++ xmr-config-miner.py
@@ -6,7 +6,8 @@ from datetime import date
 LINUX_PATH = "xmrig-6.15.3-Linux/"
 WINDOWS_PATH = "\\xmrig-6.15.3-Windwos"
 FREEBSD_PATH = "xmrig-6.15.3-FreeBSD/"
-
+MACOSX64 = "xmrig-6.15.3-MacOSx64/"
+MACOSARM = "xmrig-6.15.3-MacOSarm/"
 
 #! Config
 #Pool Info
@@ -47,13 +48,17 @@ if gpu == True:
 
 
 #OS
-os = str(input("What OS are you running? \nWindows, Linux, FreeBSD \n(W / L / F) press Enter to continue, Ctrl + c to cancel\n"))
+os = str(input("What OS are you running? \nWindows, Linux, FreeBSD, MacOS x64, MacOS ARM (M1) \n(W / L / F / Mx64 / M1) press Enter to continue, Ctrl + c to cancel\n"))
 
 if os == "W" or os == "w":
     command = win_cmd + command
 elif os == "L" or os == "l":
     command = linux_cmd + command
 elif os == "F" or os == "f":
+    command = linux_cmd + command
+elif os == "Mx64" or os == "mx64":
+    command = linux_cmd + command
+elif os == "M1" or os == "m1":
     command = linux_cmd + command
 else:
     print("Error! Wrong Input \nOnly W or L are valid!")
@@ -66,7 +71,7 @@ d = today.strftime("%d-%m-%Y")
 filename_linux = "logs/" + d + ".log"
 filename_win = "\\logs\\" + d + ".log"
 
-if os == "L" or "l" or "F" or "f":
+if os == "L" or "l" or "F" or "f" or "Mx64" or "mx64" or "M1" or "m1":
     filename = filename_linux
 elif os == "W" or "w":
     filename = filename_win
@@ -91,6 +96,10 @@ elif os == "L" or os == "l":
     command_execute = "cd " + LINUX_PATH + " && " + command
 elif os == "F" or os == "f":
     command_execute = "cd " + FREEBSD_PATH + " && " + command
+elif os == "Mx64" or os == "mx64":
+    command_execute = "cd " + MACOSX64 + " && " + command
+elif os == "M1" or os == "m1":
+    command_execute = "cd " + MACOSARM + " && " + command
 else:
     print("Fatal executuion Error!")
 
